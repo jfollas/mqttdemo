23c95573080d59f07f4988ef8993212f14c21c6e
jfollas <jasonfollas@quickenloans.com>
2018-01-24 11:11:06 -0500

    Changed formatting of broker console output

--- broker/index.js
+++ broker/index.js
@@ -29,7 +29,7 @@ server.on('clientConnected', function(client) {
  
 // fired when a message is received 
 server.on('published', function(packet, client) {
-  console.log('Published', packet.payload);
+  console.log('Published:', packet.topic, `QoS=${packet.qos}`, packet.payload.toString());
 });
  
 server.on('ready', setup);
